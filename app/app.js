e3e7bde1a9b565dbf057ec5ce03e9b88f1f5e535
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2014-10-21 00:41:41 -0400

    A simple demo on `hello meteoroid` page
    Demo is implemented using meteor’s ReactiveVar. Nothing complex, but
    reactivity is working. Proof of concept.

--- app/app.js
+++ app/app.js
@@ -1,2 +1,26 @@
-/* Export everything into the global scope! */
+/* Import everything into the global scope! */
 require('./packages/meteoroid.js')(this);
+
+/* Import dependencies */
+
+
+
+/* UI Code */
+$(document).ready(function () {
+  var myName = new ReactiveVar('Meteoroid');
+
+  Tracker.autorun(function () {
+    $('#greeting').text('Hello, ' + myName.get() + '!');
+  });
+
+  $('#reload').click(function () {
+    document.location.reload();
+  });
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+    var name = $('#name').val();
+    if (name === '') name = 'Meteoroid';
+    myName.set(name);
+  });
+});
