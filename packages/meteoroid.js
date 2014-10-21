c6ce4bfd9ea7a8b6d5effc44633f03028cafeb9a
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2014-10-21 14:20:04 -0400

    Added Session variable
    …it was defined in the meteor:session package, but I thought it would
    be better to add an alias in the meteoroid file.

--- packages/meteoroid.js
+++ packages/meteoroid.js
@@ -1,17 +1,29 @@
 /* ----------------------------------------------------------------------------
-    Main Meteoroid import file.
+    Main Meteoroid script.
     Exports the essential Meteor packages.
    ---------------------------------------------------------------------------- */
 _ = require('underscore');
 
 /* Import various stuff */
 module.exports = function (scope) {
+
+  /* Export meteor packages */
   _.extend(scope, {
     Tracker:        require('./meteor/tracker/tracker.js'),
     ReactiveVar:    require('./meteor/reactive-var/reactive-var.js'),
+    ReactiveDict:   require('./meteor/reactive-dict/reactive-dict.js'),
 
     /* jQuery */
     jQuery:         require('./jquery/jquery/jquery.min.js'),
     $:              require('./jquery/jquery/jquery.min.js')
   });
+
+  /* Add aliases */
+  _.extend(scope, {
+    $:              scope.jQuery,
+    Session:        new scope.ReactiveDict()
+  });
+
+
+
 };
